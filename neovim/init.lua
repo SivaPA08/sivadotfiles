28309dd23e81e812a6772be002e5d62d0b049903
Siva <164894350+SivaPA08@users.noreply.github.com>
2026-06-14 16:37:49 +0530

    Delete neovim directory

--- neovim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.tabspace")
-require("config.cursor")
-require("config.tabline")
--- neovim/lua/config/cursor.lua
+++
@@ -1,1 +0,0 @@
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:block,t:block"
